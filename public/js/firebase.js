f93e28604e04d7b09596ccfb6307a9d4e7748e29
armanot <armanot@gmail.com>
2025-02-01 22:00:42 +0800

    firebase details hide

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,17 +1,28 @@
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import dotenv from "dotenv";
 
-// Firebase config
+dotenv.config();
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCj8I5s4UZGywy6ZOKgGudCWSgc-uVhpto",
-    authDomain: "wisdomlink-7829b.firebaseapp.com",
-    projectId: "wisdomlink-7829b",
-    storageBucket: "wisdomlink-7829b.firebasestorage.app",
-    messagingSenderId: "618786969339",
-    appId: "1:618786969339:web:186cab6ab9e1b7c22cc25e",
-    measurementId: "G-49QX0MDST9"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
+
+
+
+export async function fetchUsers() {
+    const usersCollection = collection(db, "users");
+    const snapshot = await getDocs(usersCollection);
+    return snapshot.docs.map(doc => doc.data());
+}
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
